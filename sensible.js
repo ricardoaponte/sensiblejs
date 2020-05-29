73a510e0a5457e58aa7cf61dc261175c7cc9443c
ricardo <ricardo@rayvenues.com>
2020-05-29 19:46:42 -0400

    Complete redesign

--- sensible.js
+++ sensible.js
@@ -3,80 +3,82 @@ const sensible = (store) => {
     init(store);
 
     function init(store) {
-        for (const variable in store.data()) {
+        store.datosTemp = {};
+        let initializing = true;
+        Object.keys(store.data()).forEach(function(variable) {
             try {
-                Object.defineProperty(this, variable, {
+                Object.defineProperty(window, variable, {
                     get: function () {
-                        return new Function('"use strict";return ' + store.localPrefix + variable + ';')();
+                        //return new Function('"use strict";return window.' + variable + ';')();
+                        return store.datosTemp[variable];
                     },
                     set: function (value) {
-                        // Set the internal variable
-                        new Function('"use strict";' + store.localPrefix + variable + ' = ' + JSON.stringify(value) + ';')();
                         // Persist it if needed
                         if (store.persist) {
-                            // Check that the variable has a type
-                            new Function('"use strict";' + 'localStorage.setItem("' + store.localPrefix + variable + '",' + JSON.stringify(value) + ');')();
+                           // Check that the variable has a type
+                           localStorage.setItem(store.localPrefix + variable, value);
+                       }
+                        store.datosTemp[variable] = value;
+
+                        if (!initializing) {
+                            //Process data binding for elements
+                            document.querySelectorAll([`[s-bind=${variable}]`]).forEach((element) => {
+                                setElement(element);
+                                // Process display of elements
+                                ifElements();
+                                // Process appearance of elements
+                                cssElements();
+                            })
+
                         }
-
-                        // Process data binding for elements
-                        document.querySelectorAll([`[s-bind=${variable}]`]).forEach((element) => {
-                            setElement(element);
-                        })
-
-                        // Process display of elements
-                        ifElements();
-                        // Process appearance of elements
-                        cssElements();
                     }
                 });
             } catch (error) {
                 console.error(error.message);
             }
-
+        });
+        Object.keys(store.data()).forEach(function(variable) {
             // Initialize store data as internal variables
-            let dataSource;
+            let dataSource = null;
             if (store.persist) {
-                dataSource = localStorage.getItem(store.localPrefix + JSON.stringify(variable).replace(/['"]+/g, ''));
+                dataSource = localStorage.getItem(JSON.stringify(store.localPrefix + variable).replace(/['"]+/g, ''));
                 try {
                     dataSource = JSON.parse(dataSource);
                 } catch (error) {
 
                 }
-            } else {
-                dataSource = store.data()[variable];
             }
 
             let internalValue;
 
-            // Validate the variable type
-            if (dataSource == null && store.data()[variable].hasOwnProperty('default')) {
-                // Get the default value
-                internalValue = JSON.stringify(store.data()[variable].default);
-            } else {
-                // No casting information available set to direct value assignment
-                if (dataSource != null) {
-                    internalValue = JSON.stringify(dataSource);
+            if (dataSource === null) {
+                if (store.data()[variable].hasOwnProperty('default')) {
+                    internalValue = store.data()[variable].default;
                 } else {
-                    internalValue = JSON.stringify(store.data()[variable]);
+                    internalValue = store.data()[variable];
                 }
+            } else {
+                internalValue = dataSource;
             }
 
             if (store.data()[variable].hasOwnProperty('type')) {
                 // Cast the value based on data type
-                new Function(`${store.localPrefix}${variable} = store.data()['${variable}'].type(${internalValue})`)();
+                window[variable] = store.data()[variable].type(internalValue);
             } else {
                 // No casting information available
-                new Function(`${store.localPrefix}${variable} = ${internalValue}`)();
+                window[variable] = internalValue;
             }
-        }
+        })
+        initializing = false;
+
+        // Model bindings
+        modelBindings();
+        // Element visibility
+        ifElements();
+        // Element appearance
+        cssElements()
     }
 
-    // Model bindings
-    modelBindings();
-    // Element visibility
-    ifElements();
-    // Element appearance
-    cssElements()
 
     /**
      * Initialize existing elements with store data
@@ -99,44 +101,52 @@ const sensible = (store) => {
         switch (element.type) {
             case "select-one":
                 element.onchange = function (event) {
-                    new Function('"use strict";var value = ' + JSON.stringify(event.target.value) + ';' + store.localPrefix + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
+                    new Function('"use strict";var value = ' + JSON.stringify(event.target.value) + ';' + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
                 }
-                element.value = Function('"use strict";return(' + store.localPrefix + element.attributes['s-bind'].value + ');')();
+                element.value = window[element.attributes['s-bind'].value];
                 break;
             case "radio":
                 element.onchange = function (event) {
-                    new Function('"use strict";var value = ' + JSON.stringify(event.target.value) + ';' + store.localPrefix + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
+                    window[element.attributes['s-bind'].value] = event.target.value;
+                    //new Function('"use strict";var value = ' + JSON.stringify(event.target.value) + ';' + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
                 }
-                element.checked = Function('"use strict";return(' + JSON.stringify(element.value) + ' == ' + store.localPrefix + element.attributes['s-bind'].value + ');')();
+                if (element.attributes['s-bind'].value === element.id) {
+                    element.value = window[element.attributes['s-bind'].value];
+                };
+                element.checked = window[element.attributes['s-bind'].value] === element.value;
                 break;
             case "checkbox":
                 element.onchange = function (event) {
-                    new Function('"use strict";var value = ' + JSON.stringify(event.target.checked) + ';' + store.localPrefix + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
+                    window[element.attributes['s-bind'].value] = event.target.checked;
+                    //new Function('"use strict";var value = ' + JSON.stringify(event.target.checked) + ';' + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
                 }
-                element.checked = Function('"use strict";return(' + store.localPrefix + element.attributes['s-bind'].value + ');')();
+                element.checked = window[element.attributes['s-bind'].value];
                 break;
             case "text":
             case "email":
             case "textarea":
                 element.onkeyup = function (event) {
-                    new Function('"use strict";var value = ' + JSON.stringify(event.target.value) + ';' + store.localPrefix + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
+                    window[element.attributes['s-bind'].value] = event.target.value;
+                    //new Function('"use strict";var value = ' + JSON.stringify(event.target.value) + ';' + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
                 };
-                element.value = Function('"use strict";return(' + store.localPrefix + element.attributes['s-bind'].value + ');')();
+                //element.value = Function('"use strict";return(' + element.attributes['s-bind'].value + ');')();
+                element.value = window[element.attributes['s-bind'].value];
                 setInner = true;
                 break;
             case "color":
             case "date":
             case "datetime-local":
                 element.oninput = function (event) {
-                    new Function('"use strict";var value = ' + JSON.stringify(event.target.value) + ';' + store.localPrefix + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
+                    new Function('"use strict";var value = ' + JSON.stringify(event.target.value) + ';' + element.attributes['s-bind'].value + ' = ' + element.attributes['s-bind'].value + ' = value;')();
                 };
-                element.value = Function('"use strict";return ' + store.localPrefix + element.attributes['s-bind'].value + ';')();
+                element.value = window[element.attributes['s-bind'].value];
                 setInner = true;
                 break;
             case undefined:
                 if (!element.hasOwnProperty('original')) {
                     element.original = element.innerHTML;
                 }
+                element.innerHTML = window[element.attributes['s-bind'].value];
                 if (element.original !== '') {
                     let code = element.original.substr(element.original.indexOf('{{') + 2, element.original.indexOf('}}') - 2)
                     // If there is code found then process it!
@@ -154,12 +164,13 @@ const sensible = (store) => {
                         }
                     }
                 } else {
-                    element.innerHTML = new Function('"use strict";return ' + store.localPrefix + element.attributes['s-bind'].value)();
+                    //element.innerHTML = new Function('"use strict";return ' + element.attributes['s-bind'].value)();
+                    element.innerHTML = window[element.attributes['s-bind'].value];
                 }
                 break;
         }
         if (setInner) {
-            element.innerHTML = new Function('"use strict";return ' + store.localPrefix + element.attributes['s-bind'].value)();
+            element.innerHTML = window[element.attributes['s-bind'].value];
         }
     }
 
@@ -169,7 +180,8 @@ const sensible = (store) => {
     function ifElements() {
         document.querySelectorAll(['[s-if]']).forEach((element) => {
             try {
-                const display = new Function('"use strict";' + ' return ' + store.localPrefix + element.getAttribute('s-if') + ';')()
+                //TODO: Evaluate code inside
+                const display = new Function('"use strict";' + ' return ' + element.getAttribute('s-if') + ';')();
                 if (!element.hasOwnProperty('originalDisplay')) {
                     element.originalDisplay = element.style.display;
                 }
@@ -195,4 +207,132 @@ const sensible = (store) => {
             }
         })
     }
+
+    /**
+     * Process for directive
+     */
+    function forElements() {
+        document.querySelectorAll(['[s-for]']).forEach((element) => {
+            try {
+                if (!element.hasOwnProperty('original')) {
+                    element.original = element.innerHTML;
+                }
+                let forloop = element.getAttribute('s-for');
+                if (element.original !== '') {
+                    let code = element.original.substr(element.original.indexOf('{{') + 2, element.original.indexOf('}}') - 2)
+                    // If there is code found then process it!
+                    if (code && code.length > 1) {
+                        try {
+                            let nonCodeNode1Location = element.original.indexOf('{{');
+                            let nonCodeNode1 = element.original.substring(0, element.original.indexOf('{{'));
+                            let nonCodeNode2Location = element.original.indexOf('}}') + 2;
+                            let nonCodeNode2 = element.original.substring(element.original.indexOf('}}') + 2);
+                            let code = element.original.substr(nonCodeNode1Location + 2, nonCodeNode2Location - nonCodeNode1Location - 4);
+                            //let codeValue = new Function('"use strict";return ' + code + ';')();
+                            //let insertElement = `${nonCodeNode1}${codeValue}${nonCodeNode2}`;
+                            newElement = element.cloneNode();
+                            newElement.removeAttribute('s-for');
+                            let fn = `
+                            
+                            for(${forloop}) {
+                                this.newElement.insertAdjacentElement('afterend', this.newElement);
+                            }
+                            `;
+                            new Function(fn)();
+                            element.remove();
+                            //element.innerHTML = `${nonCodeNode1}${codeValue}${nonCodeNode2}`;
+                        } catch (error) {
+                            console.error(error.message);
+                        }
+                    }
+                } else {
+                    element.innerHTML = new Function('"use strict";return ' + element.attributes['s-bind'].value)();
+                }
+            } catch (error) {
+                console.error(error.message);
+            }
+        })
+    }
+
+    function ArrayObserver(a){
+        var _this = this;
+        this.array = a;
+        this.observers = [];
+
+        this.Observe = function (notifyCallback){
+            _this.observers.push(notifyCallback);
+        }
+
+        a.push = function(obj){
+            var push = Array.prototype.push.apply(a, arguments);
+            for(var i = 0; i < _this.observers.length; i++) _this.observers[i](obj, "push");
+            return push;
+        }
+
+        a.pop = function(){
+            var popped = Array.prototype.pop.apply(a, arguments);
+            for(var i = 0; i < _this.observers.length; i++) _this.observers[i](popped, "pop");
+            return popped;
+        }
+
+        a.reverse = function() {
+            var result = Array.prototype.reverse.apply(a, arguments);
+            for(var i = 0; i < _this.observers.length; i++) _this.observers[i](result, "reverse");
+            return result;
+        };
+
+        a.shift = function() {
+            var deleted_item = Array.prototype.shift.apply(a, arguments);
+            for(var i = 0; i < _this.observers.length; i++) _this.observers[i](deleted_item, "shift");
+            return deleted_item;
+        };
+
+        a.sort = function() {
+            var result = Array.prototype.sort.apply(a, arguments);
+            for(var i = 0; i < _this.observers.length; i++) _this.observers[i](result, "sort");
+            return result;
+        };
+
+        a.splice = function(i, length, itemsToInsert) {
+            var returnObj
+            if(itemsToInsert){
+                Array.prototype.slice.call(arguments, 2);
+                returnObj = itemsToInsert;
+            }
+            else{
+                returnObj = Array.prototype.splice.apply(a, arguments);
+            }
+            for(var i = 0; i < _this.observers.length; i++) _this.observers[i](returnObj, "splice");
+            return returnObj;
+        };
+
+        a.unshift = function() {
+            var new_length = Array.prototype.unshift.apply(a, arguments);
+            for(var i = 0; i < _this.observers.length; i++) _this.observers[i](new_length, "unshift");
+            return arguments;
+        };
+    }
+
+    function Observer(o, property){
+        var _this = this;
+        var value = o[property];
+        this.observers = [];
+
+        this.Observe = function (notifyCallback){
+            _this.observers.push(notifyCallback);
+        }
+
+        Object.defineProperty(o, property, {
+            set: function(val){
+                _this.value = val;
+                for(var i = 0; i < _this.observers.length; i++) _this.observers[i](val);
+            },
+            get: function(){
+                return _this.value;
+            }
+        });
+    }
+
+
 }
+exports = sensible;
