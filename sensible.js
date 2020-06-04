42aa30775ad8a35cc6df44ffa4a8ccfeb8d88872
ricardo <ricardo@rayvenues.com>
2020-06-04 10:59:40 -0400

    Bug fix

--- sensible.js
+++ sensible.js
@@ -2,29 +2,6 @@
 
 function sensible(store) {
 
-    // const storeTemplate = {
-    //     persist: true,
-    //     localPrefix: '__',
-    //     private: false,
-    //     data: {},
-    // };
-
-    function getIds() {
-        let ids = [];
-        for (let variable of document.querySelectorAll('[id]')) {
-            ids.push(variable.id);
-        }
-        console.log(ids);
-    }
-
-    function hasCode(value) {
-        return value.substr(value.indexOf('[[') + 2, value.indexOf(']]') - 2).length > 0
-    }
-
-    function getCode(value) {
-        return value.replace(/\[\[/g, "' + ").replace(/\]\]/g, " + '").replace(/(\r\n|\n|\r)/gm, "");
-    }
-
     init(store);
 
     function init(store) {
@@ -42,8 +19,7 @@ function sensible(store) {
                         }
                     }
                     if (!initializing) {
-                        updateAll();
-                        executeCallBack(variable);
+                        processElements(variable);
                     }
                 });
             } else if (store.data[variable].hasOwnProperty('type') && store.data[variable].type === Object) {
@@ -51,8 +27,7 @@ function sensible(store) {
                 const observer = new Observer(window, variable, variable);
                 observer.Observe(function (value) {
                     if (!initializing) {
-                        updateAll();
-                        executeCallBack(variable);
+                        processElements(variable);
                     }
                 })
                 Object.keys(store.data[variable].default).forEach(function (property) {
@@ -60,8 +35,7 @@ function sensible(store) {
                     const observer = new Observer(window[variable], property, variable);
                     observer.Observe(function (value) {
                         if (!initializing) {
-                            updateAll();
-                            executeCallBack(variable);
+                            processElements(variable);
                         }
                     })
                 });
@@ -70,14 +44,19 @@ function sensible(store) {
                 const observer = new Observer(window, variable, false);
                 observer.Observe(function (value) {
                     if (!initializing) {
-                        updateAll();
-                        executeCallBack(variable);
+                        processElements(variable);
                     }
                 })
             }
-            let dataSource = null;
-            let currentVariable = store.data[variable];
-            if (store.persist || (store.data[variable].hasOwnProperty('persist') && store.data[variable].persist === true)) {
+            let dataSource = null, currentVariable = store.data[variable];
+            if (store.persist) {
+                dataSource = localStorage.getItem(store.localPrefix + variable);
+                try {
+                    dataSource = JSON.parse(dataSource);
+                } catch (error) {
+                    //console.error(error);
+                }
+            } else if (store.data[variable].hasOwnProperty('persist') && store.data[variable].persist === true) {
                 dataSource = localStorage.getItem(store.localPrefix + variable);
                 try {
                     dataSource = JSON.parse(dataSource);
@@ -141,11 +120,13 @@ function sensible(store) {
         // Model bindings
         modelBindings();
         // Element display
-        ifElements();
-        // Element appearance
-        cssElements()
-        // Elements with for loops
-        forElements();
+        document.querySelectorAll("[s-if]").forEach((element) => {
+            ifElement(element);
+        });
+        // Element FOR
+        document.querySelectorAll("[s-for]").forEach((element) => {
+            forElement(element);
+        });
     }
 
     /**
@@ -154,17 +135,6 @@ function sensible(store) {
     function modelBindings() {
         // Model bindings
         let elements = document.querySelectorAll(['[s-bind]']);
-        elements.forEach((element) => {
-            setElement(element);
-        })
-    }
-
-    /**
-     * Initialize existing elements with store data directives
-     */
-    function bindElements(variable) {
-        // Elements
-        let elements = document.querySelectorAll([`[s-bind*=${variable}]`]);
         elements.forEach((element) => {
             setElement(element);
         })
@@ -274,147 +244,104 @@ function sensible(store) {
         }
     }
 
-    function processElements() {
-        let elements = [];
-        Object.keys(store.data).forEach(function (variable) {
-            document.querySelectorAll("[s-bind]").forEach((element) => {
-                if (element.innerHTML.indexOf(variable) >= 0) {
-                    if (elements.indexOf(element) === -1) {
-                        elements.push(element);
-                    }
-                }
-            });
-            bindElements(elements);
-            elements = [];
-            document.querySelectorAll("[s-for]").forEach((element) => {
-                if (element.innerHTML.indexOf(variable) >= 0) {
-                    if (elements.indexOf(element) === -1) {
-                        elements.push(element);
-                    }
-                }
-            });
-            bindElements(elements);
-            elements = [];
-            document.querySelectorAll("[s-if]").forEach((element) => {
-                if (element.innerHTML.indexOf(variable) >= 0) {
-                    if (elements.indexOf(element) === -1) {
-                        elements.push(element);
-                    }
-                }
-            });
-            ifElements(elements);
-            elements = [];
-            document.querySelectorAll("[c-css]").forEach((element) => {
-                if (element.innerHTML.indexOf(variable) >= 0) {
-                    if (elements.indexOf(element) === -1) {
-                        elements.push(element);
-                    }
-                }
-            });
-            cssElements(elements);
+    function processElements(variable) {
+        document.querySelectorAll(`[s-bind],[s-bind*=${variable}]`).forEach((element) => {
+            if (element.innerHTML.indexOf(variable) >= 0 || element.getAttribute('s-bind') === variable) {
+                setElement(element);
+            }
         });
 
-        // document.querySelectorAll("[s-bind],[s-for],[s-if],[s-blur]").forEach((element) => {
-        //     Object.keys(store.data).forEach(function (variable) {
-        //         if (element.innerHTML.indexOf(variable) >= 0) {
-        //             if (elements.indexOf(element) === -1) {
-        //                 elements.push(element);
-        //             }
-        //         }
-        //     });
-        // });
-        // console.log(elements);
+        document.querySelectorAll("[s-for]").forEach((element) => {
+            if (element.innerHTML.indexOf(variable) >= 0 || element.getAttribute('s-for') === variable) {
+                forElement(element);
+            }
+        });
+
+        document.querySelectorAll("[s-if]").forEach((element) => {
+            if (element.innerHTML.indexOf(variable) >= 0 || element.getAttribute('s-if') === variable) {
+                ifElement(element);
+            }
+        });
+
+        document.querySelectorAll("[s-css]").forEach((element) => {
+            if (element.innerHTML.indexOf(variable) >= 0 || element.getAttribute('s-css') === variable) {
+                cssElement(element);
+            }
+        });
+        executeCallBack(variable);
     }
 
     /**
      * Set elements sensible visibility
      */
-    function ifElements(variable) {
-        let selector = ['[s-if]'];
-        if (variable) {
-            selector = [`[s-if*=${variable}]`];
-        }
-
-        document.querySelectorAll(selector).forEach((element) => {
-            try {
-                //TODO: Evaluate code inside
-                const display = exec(element.getAttribute('s-if'));
-                if (!element.hasOwnProperty('originalDisplay')) {
-                    element.originalDisplay = element.style.display;
-                }
-                // Preserve original display
-                element.style.display = display ? element.originalDisplay : 'none';
-            } catch (error) {
-                console.error(error.message);
+    function ifElement(element) {
+        try {
+            //TODO: Evaluate code inside
+            const display = exec(element.getAttribute('s-if'));
+            if (!element.hasOwnProperty('originalDisplay')) {
+                element.originalDisplay = element.style.display;
             }
-        })
+            // Preserve original display
+            element.style.display = display ? element.originalDisplay : 'none';
+        } catch (error) {
+            console.error(error.message);
+        }
     }
 
     /**
      * Set elements sensible appearance
      */
-    function cssElements(variable) {
-        let selector = ['[s-css]'];
-        if (variable) {
-            selector = [`[s-css*=${variable}]`];
+    function cssElement(element) {
+        try {
+            element.getAttribute('s-css').split(';').forEach(function (style) {
+                //Object.assign(element.style, new Function(`return {"${style.split(':')[0].trim()}":${style.split(':')[1].trim()}}`)());
+                Object.assign(element.style, exec(`{"${style.split(':')[0].trim()}":${style.split(':')[1].trim()}}`));
+            });
+        } catch (error) {
+            console.error(error.message);
         }
-        document.querySelectorAll([selector]).forEach((element) => {
-            try {
-                element.getAttribute('s-css').split(';').forEach(function (style) {
-                    //Object.assign(element.style, new Function(`return {"${style.split(':')[0].trim()}":${style.split(':')[1].trim()}}`)());
-                    Object.assign(element.style, exec(`{"${style.split(':')[0].trim()}":${style.split(':')[1].trim()}}`));
-                });
-            } catch (error) {
-                console.error(error.message);
-            }
-        })
     }
 
     /**
      * Process FOR directive
      */
-    function forElements(variable) {
-        let selector = ['[s-for]'];
-        if (variable) {
-            selector = [`[s-for*=${variable}]`];
-        }
-        document.querySelectorAll(['[s-for]']).forEach((element) => {
-            try {
-                // If the original node is already set then assign it to the current element
-                let templateElement;
-                let parentElement;
-                if (element.hasOwnProperty('templateElement')) {
-                    templateElement = element.templateElement.cloneNode(true);
-                    parentElement = element;
-                } else if (element.parentElement && !element.parentElement.hasOwnProperty('originalNode')) {
-                    // Save the original element and directive attributes inside it's parent element
-                    element.parentElement.templateElement = element.cloneNode(true);
-                    element.parentElement.setAttribute('s-for', element.getAttribute('s-for'));
-                    element.parentElement.setAttribute('s-key', element.getAttribute('s-key'));
-                    // Set the template element to its parents' template element
-                    templateElement = element.parentElement.templateElement;
-                    parentElement = element.parentElement;
-                } else {
-                    console.log('Unexpected route...');
-                    return;
-                }
-                let forloop = templateElement.getAttribute('s-for');
-                // Will we need a key?
-                // let key = templateElement.getAttribute('s-key');
-                if (templateElement.innerHTML !== '') {
-                    // If there is code found then process it!
-                    if (hasCode(templateElement.innerHTML)) {
-                        try {
-                            let value = '';
-                            let innerHTML = getCode("'" + templateElement.innerHTML + "'");
-                            // TODO: Evaluate other elements like OPTIONS or a different way to evaluate this
-                            if (templateElement.tagName === 'OPTION') {
-                                let code = getCode(templateElement.value);
-                                if (code && code.length > 1) {
-                                    value = getCode(templateElement.value);
-                                }
+    function forElement(element) {
+        try {
+            // If the original node is already set then assign it to the current element
+            let templateElement;
+            let parentElement;
+            if (element.hasOwnProperty('templateElement')) {
+                templateElement = element.templateElement.cloneNode(true);
+                parentElement = element;
+            } else if (element.parentElement && !element.parentElement.hasOwnProperty('originalNode')) {
+                // Save the original element and directive attributes inside it's parent element
+                element.parentElement.templateElement = element.cloneNode(true);
+                element.parentElement.setAttribute('s-for', element.getAttribute('s-for'));
+                element.parentElement.setAttribute('s-key', element.getAttribute('s-key'));
+                // Set the template element to its parents' template element
+                templateElement = element.parentElement.templateElement;
+                parentElement = element.parentElement;
+            } else {
+                console.log('Unexpected route...');
+                return;
+            }
+            let forloop = templateElement.getAttribute('s-for');
+            // Will we need a key?
+            // let key = templateElement.getAttribute('s-key');
+            if (templateElement.innerHTML !== '') {
+                // If there is code found then process it!
+                if (hasCode(templateElement.innerHTML)) {
+                    try {
+                        let value = '';
+                        let innerHTML = getCode("'" + templateElement.innerHTML + "'");
+                        // TODO: Evaluate other elements like OPTIONS or a different way to evaluate this
+                        if (templateElement.tagName === 'OPTION') {
+                            let code = getCode(templateElement.value);
+                            if (code && code.length > 1) {
+                                value = getCode(templateElement.value);
                             }
-                            let fn = `
+                        }
+                        let fn = `
                                 var index = 0;
                                 var newElements = [];
                                 // Create new elements
@@ -422,9 +349,11 @@ function sensible(store) {
                                     let newElement = templateElement.cloneNode(true);
                                     newElement.removeAttribute('s-for');
                                     newElement.removeAttribute('s-key');
-                                    newElement.innerHTML = new Function('"use strict";return' + innerHTML + ';')();
+                                    let fn = new Function('index', '"use strict";return' + innerHTML + ';');
+                                    newElement.innerHTML = fn(index);
                                     if (value !== '' && value !== undefined && value !== 'undefined' ) {
-                                        newElement.value = new Function('"use strict";return ' + value + ';')();
+                                       let fn = new Function('index', '"use strict";return' + value + ';');
+                                       newElement.value = fn(index);
                                     }
                                     // Assign index to s-key-value attribute
                                     let attribute = document.createAttribute("s-key-value");
@@ -444,17 +373,42 @@ function sensible(store) {
                                     parentElement.appendChild(newElement);
                                 }
                                 `;
-                            let func = new Function('parentElement', 'templateElement', 'innerHTML', 'value', fn);
-                            func(parentElement, templateElement, innerHTML, value);
-                        } catch (error) {
-                            console.error(error.message);
-                        }
+                        let func = new Function('parentElement', 'templateElement', 'innerHTML', 'value', fn);
+                        func(parentElement, templateElement, innerHTML, value);
+                    } catch (error) {
+                        console.error(error.message);
                     }
                 }
-            } catch (error) {
-                console.error(error.message);
             }
-        })
+        } catch (error) {
+            console.error(error.message);
+        }
+    }
+
+    /**
+     * Verify that the value contains code
+     * @param value
+     * @returns {boolean}
+     */
+    function hasCode(value) {
+        return value.substr(value.indexOf('[[') + 2, value.indexOf(']]') - 2).length > 0
+    }
+
+    /**
+     * Get the code from the value
+     * @param value
+     * @returns {string}
+     */
+    function getCode(value) {
+        return value.replace(/\[\[/g, "' + ").replace(/\]\]/g, " + '").replace(/(\r\n|\n|\r)/gm, "");
+    }
+    /**
+     * Execute code and return result
+     * @param value
+     * @returns {*}
+     */
+    function exec(value) {
+        return new Function('"use strict";return ' + value + ';')();
     }
 
     /**
@@ -530,15 +484,6 @@ function sensible(store) {
     }
 
     /**
-     * Execute code and return result
-     * @param value
-     * @returns {*}
-     */
-    function exec(value) {
-        return new Function('"use strict";return ' + value + ';')();
-    }
-
-    /**
      * Original Code by Blaize Stewart, Aug 7, 2019
      * @param o
      * @param property
@@ -573,6 +518,25 @@ function sensible(store) {
             }
         });
     }
+
+    // const storeTemplate = {
+    //     persist: true,
+    //     localPrefix: '__',
+    //     private: false,
+    //     data: {},
+    // };
+
+    /**
+     * Initiate existing id recognition.
+     */
+    function getIds() {
+        let ids = [];
+        for (let variable of document.querySelectorAll('[id]')) {
+            ids.push(variable.id);
+        }
+        console.log(ids);
+    }
+
 }
 
 // Adapt for either browser or not. AMD support
